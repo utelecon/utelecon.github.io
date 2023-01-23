4b22efa434f7d2f0ce1be3e13d8fd3ec36a6c82d
cm-ayf <cm.ayf2734@gmail.com>
2023-01-23 13:53:18 +0900

    add DefaultLayoutPlugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import mdx from "@astrojs/mdx";
 import { defineConfig } from "astro/config";
-import path from "path";
+import defaultLayoutPlugin from "./src/lib/DefaultLayoutPlugin.mjs";
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    remarkPlugins: [
+      [defaultLayoutPlugin, { defaultLayout: "@layouts/Layout.astro" }],
+    ],
+  },
   integrations: [mdx()],
 });
