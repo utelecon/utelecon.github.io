5b08d4c3f4c63b4e301056f5435e144dd8853ad6
cm-ayf <cm.ayf2734@gmail.com>
2023-01-23 15:50:48 +0900

    add toc

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,16 @@ import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import { defineConfig } from "astro/config";
 import blockIALPlugin from "./src/lib/BlockIALPlugin.mjs";
-import defaultLayoutPlugin from "./src/lib/DefaultLayoutPlugin.mjs";
+import defaultFrontmatterPlugin from "./src/lib/DefaultFrontmatterPlugin.mjs";
+import insertTocPlugin from "./src/lib/InsertTocPlugin.mjs";
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
     remarkPlugins: [
-      [defaultLayoutPlugin, { defaultLayout: "@layouts/Layout.astro" }],
+      [defaultFrontmatterPlugin, { layout: "@layouts/Layout.astro" }],
       blockIALPlugin,
+      insertTocPlugin,
     ],
   },
   integrations: [mdx(), react()],
--- src/lib/DefaultLayoutPlugin.mjs
+++
@@ -1,9 +0,0 @@
-/**
- * https://github.com/withastro/astro/issues/397#issuecomment-1236231783
- * @type {import('unified').Plugin<[options: { defaultLayout: string }]>}
- */
-export default function defaultLayoutPlugin({ defaultLayout }) {
-  return function (_, file) {
-    file.data.astro.frontmatter.layout ??= defaultLayout;
-  };
-}
