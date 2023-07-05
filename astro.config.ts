3e4e70489a75c10797c7ce4d6f6b26df096ea207
cm-ayf <cm.ayf2734@gmail.com>
2023-07-05 18:02:35 +0900

    process trailing slash

--- astro.config.ts
+++ astro.config.ts
@@ -7,6 +7,7 @@ import blockIALPlugin from "./src/lib/BlockIALPlugin.js";
 import defaultFrontmatterPlugin from "./src/lib/DefaultFrontmatterPlugin.js";
 import simpleAttentionPlugin from "./src/lib/SimpleAttentionPlugin.js";
 import externalLinks from "./src/lib/ExternalLinksIntegration.js";
+import trailingSlash from "./src/lib/TrailingSlashIntegration.js";
 
 // https://astro.build/config
 export default defineConfig({
@@ -31,6 +32,7 @@ export default defineConfig({
     react(),
     redirect(),
     externalLinks({ target: "_blank", rel: ["noopener", "noreferrer"] }),
+    trailingSlash(),
   ],
   site: "https://utelecon.adm.u-tokyo.ac.jp",
 });
