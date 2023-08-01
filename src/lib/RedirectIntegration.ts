318790f56c910e7b19fe117fed33247781f28404
Futa IWATA <futaiwata@g.ecc.u-tokyo.ac.jp>
2023-08-01 19:48:17 +0900

    fix redirect

--- src/lib/RedirectIntegration.ts
+++ src/lib/RedirectIntegration.ts
@@ -22,10 +22,12 @@ export default function redirect(): AstroIntegration {
         const files = await Promise.all(promises);
         const redirects = files.flatMap((file) => {
           const { redirect_to, redirect_from } = file.data;
-          const here = relative(pages, file.path).replace(
-            /(?:index)?\.(?:md|mdx|markdown|astro)$/,
-            ""
-          );
+          const here =
+            "/" +
+            relative(pages, file.path).replace(
+              /(?:index)?\.(?:md|mdx|markdown|astro)$/,
+              ""
+            );
           if (typeof redirect_to === "string") {
             return { from: here, to: redirect_to };
           }
