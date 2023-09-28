50295c3fa95123db44dc8db72b49ccdfa212c844
Futa IWATA <futaiwata@g.ecc.u-tokyo.ac.jp>
2023-09-28 20:37:36 +0900

    use estree-util-visit

--- src/lib/DotSlashPlugin.ts
+++ src/lib/DotSlashPlugin.ts
@@ -1,50 +1,26 @@
 // Prepends "./" to image paths so that it can be imported as a relative path.
 
 import type { Root } from "mdast";
+import "mdast-util-mdx";
 import { visit } from "unist-util-visit";
-import { z } from "zod";
+import { visit as estreeVisit } from "estree-util-visit";
 
 const ALLOWED_PREFIXES = ["http:", "https:", "/", "./", "../"];
 
-const nodeSchema = z.object({
-  type: z.literal("mdxjsEsm"),
-  value: z.literal(""),
-  data: z.object({
-    estree: z.object({
-      type: z.literal("Program"),
-      sourceType: z.literal("module"),
-      body: z.array(
-        z.object({
-          type: z.literal("ImportDeclaration"),
-          source: z.object({ type: z.literal("Literal"), value: z.string(), raw: z.string() }),
-          specifiers: z.array(z.object({
-            type: z.literal("ImportDefaultSpecifier"),
-            local: z.object({ type: z.literal("Identifier"), name: z.string() })
-          }))
-        })
-      )
-    })
-  })
-});
-
 export default function dotSlashPlugin() {
   return (root: Root) => {
-    // For MDX (remark-images-components)
-    const importStatements = root
-      .children
-      .flatMap(child => {
-        const parseResult = nodeSchema.safeParse(child);
-        if (!parseResult.success) return [];
-
-        const imageImport = child as unknown as typeof parseResult.data;
-        return imageImport.data.estree.body;
+    visit(root, "mdxjsEsm", (node) => {
+      const estree = node.data?.estree;
+      if (!estree) return;
+      estreeVisit(estree, (node) => {
+        if (node.type !== "ImportDeclaration") return;
+        const { value, raw } = node.source;
+        if (!raw || typeof value !== "string") return;
+        if (!ALLOWED_PREFIXES.some((prefix) => value.startsWith(prefix))) {
+          node.source.value = `./${value}`;
+          node.source.raw = JSON.stringify(`./${JSON.parse(raw)}`);
+        }
       });
-    importStatements.forEach(node => {
-      const { value, raw } = node.source;
-      if (!ALLOWED_PREFIXES.some((prefix) => value.startsWith(prefix))) {
-        node.source.value = `./${value}`;
-        node.source.raw = JSON.stringify(`./${JSON.parse(raw)}`);
-      }
     });
 
     // For md
@@ -52,6 +28,6 @@ export default function dotSlashPlugin() {
       if (!ALLOWED_PREFIXES.some((prefix) => node.url.startsWith(prefix))) {
         node.url = `./${node.url}`;
       }
-    })
+    });
   };
 }
