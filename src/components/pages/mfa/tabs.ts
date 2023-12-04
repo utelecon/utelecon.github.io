35a868f1677b7a148590bd66a65009bc3a80b953
Akira Takeuchi <aktakeuch@g.ecc.u-tokyo.ac.jp>
2023-12-04 12:27:43 +0900

    Revert "Revert "Merge branch '202309-mfa-interactive'""
    This reverts commit 93a54ef7cb39179a3b854a3c0e9b0adefbe1481e.

---
+++ src/components/pages/mfa/tabs.ts
@@ -0,0 +1,32 @@
+export type Step = "first" | "alt";
+export type Group = "intent" | Step;
+export type Selection = "select" | "ms_auth" | "auth_app" | "phone" | "fido";
+export type Labels = Partial<Record<Selection, string>>;
+
+type Listener = (step: string, selection: string) => void;
+const listeners: Listener[] = [];
+
+export function onChangeTab(listener: Listener) {
+  listeners.push(listener);
+}
+
+export function emitChangeTab(step: string, selection: string) {
+  for (const listener of listeners) {
+    listener(step, selection);
+  }
+}
+
+onChangeTab((step, selection) => {
+  const url = new URL(location.href);
+  if (selection === "selector") url.searchParams.delete(step);
+  else url.searchParams.set(step, selection);
+  history.replaceState(null, "", url.href);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  for (const step of ["first", "alt"]) {
+    const url = new URL(location.href);
+    const selection = url.searchParams.get(step) ?? "selector";
+    emitChangeTab(step, selection);
+  }
+});
