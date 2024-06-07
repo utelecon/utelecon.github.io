71d3be9db7c8d37e2d6c1f73c15c592587fbbc28
haxibami <me@haxibami.net>
2024-06-07 17:57:29 +0900

    reflect review comments

--- src/lib/CopyAssetIntegration.ts
+++ src/lib/CopyAssetIntegration.ts
@@ -1,23 +1,23 @@
 import type { AstroIntegration } from "astro";
 
-import { copyFileSync, mkdirSync, existsSync } from "node:fs";
+import { existsSync } from "node:fs";
+import { copyFile, mkdir } from "node:fs/promises";
+import { dirname, join, relative, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
-import { join, dirname, relative } from "node:path";
-import { unified } from "unified";
+import { selectAll } from "hast-util-select";
 import parse from "rehype-parse";
-import rehypeCollectAssets from "./CollectAssetsPlugin.js";
-import stringify from "rehype-stringify";
 import { read } from "to-vfile";
+import { unified } from "unified";
 
 export default function CopyAssetIntegration(): AstroIntegration {
-  const processor = unified()
-    .use(parse)
-    .use(rehypeCollectAssets)
-    .use(stringify);
+  const parser = unified().use(parse);
+
   return {
     name: "copy-asset",
     hooks: {
       "astro:build:done": async ({ dir, routes }) => {
+        let collectedPaths: string[] = [];
+
         await Promise.all(
           routes.map(async ({ pathname, component }) => {
             if (!pathname) return;
@@ -25,37 +25,50 @@ export default function CopyAssetIntegration(): AstroIntegration {
               pathname === "/404"
                 ? join(fileURLToPath(dir), "404.html")
                 : join(fileURLToPath(dir), pathname, "index.html");
-            const source = await read(path);
-            const processed = await processor.process(source);
-            const assets = processed.data.assets;
-            if (!assets) return;
 
-            const assetPaths = assets.map((assetFile) =>
-              assetFile.startsWith("/")
-                ? decodeURI(assetFile)
-                : decodeURI(
-                    join(relative("src/pages", dirname(component)), assetFile),
-                  ),
+            const source = await read(path);
+            const hast = parser.parse(source);
+            const ASSETS_PATTERN =
+              /^(?!http|javascript:|mailto:|tel:).*\.(?!html|png|jpg|jpeg)[a-zA-Z0-9]+$/;
+            const assets: string[] = [];
+
+            for (const node of selectAll("a", hast)) {
+              const href = node.properties?.href?.toString();
+              if (href && ASSETS_PATTERN.test(href)) {
+                assets.push(href);
+              }
+            }
+
+            const paths = assets.map((assetFile) =>
+              decodeURI(
+                resolve(
+                  "/",
+                  relative("src/pages", dirname(component)),
+                  assetFile,
+                ),
+              ),
             );
 
-            // copy from "src/pages/assetPath" to "dist/assetPath"
-            // create directory if it doesn't exist
-            assetPaths.map((assetPath) => {
-              const sourcePath = join(process.cwd(), "src", "pages", assetPath);
-              const destinationPath = join(fileURLToPath(dir), assetPath);
-              if (!existsSync(sourcePath)) {
-                if (!existsSync(`${sourcePath}.md`)) {
-                  console.warn(
-                    `Asset file ${sourcePath} not found: referenced in ${component}`,
-                  );
-                }
-              } else {
-                if (!existsSync(dirname(destinationPath))) {
-                  mkdirSync(dirname(destinationPath), { recursive: true });
-                }
-                copyFileSync(sourcePath, destinationPath);
+            collectedPaths = collectedPaths.concat(paths);
+          }),
+        );
+
+        const assetPaths = [...new Set(collectedPaths)];
+
+        await Promise.all(
+          assetPaths.map(async (assetPath) => {
+            const srcPath = resolve(join("src/pages", assetPath));
+            const destPath = join(fileURLToPath(dir), assetPath);
+            if (!existsSync(srcPath)) {
+              if (!existsSync(`${srcPath}.md`)) {
+                console.warn(`Asset file ${srcPath} not found`);
               }
-            });
+              return;
+            }
+            if (!existsSync(dirname(destPath))) {
+              await mkdir(dirname(destPath), { recursive: true });
+            }
+            await copyFile(srcPath, destPath);
           }),
         );
       },
