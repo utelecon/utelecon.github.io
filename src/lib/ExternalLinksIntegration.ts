2f12df78a22e5538c17d1112007524ed1a7aebc1
seieric <30688855+seieric@users.noreply.github.com>
2024-10-03 21:27:58 +0900

    refactor

--- src/lib/ExternalLinksIntegration.ts
+++ src/lib/ExternalLinksIntegration.ts
@@ -1,13 +1,11 @@
 import type { AstroIntegration } from "astro";
-import { fileURLToPath } from "url";
 import { unified } from "unified";
 import parse from "rehype-parse";
 import rehypeExternalLinks from "rehype-external-links";
 import type { Options } from "rehype-external-links";
 import stringify from "rehype-stringify";
-import { join } from "path";
 import { read, write } from "to-vfile";
-import { parse as parsePath } from "path";
+import { getDistFilePath } from "./util";
 
 export default function externalLinks(options: Options): AstroIntegration {
   const processor = unified()
@@ -22,15 +20,7 @@ export default function externalLinks(options: Options): AstroIntegration {
         await Promise.all(
           routes.map(async ({ pathname, component }) => {
             if (!pathname || pathname.endsWith("/rss.xml")) return;
-            let path =
-              pathname === "/404"
-                ? join(fileURLToPath(dir), "404.html")
-                : join(fileURLToPath(dir), pathname);
-            path =
-              parsePath(component).name == "index"
-                ? join(path, "index.html")
-                : path.replace(/(\.html)?$/, ".html");
-
+            const path = getDistFilePath(dir, pathname, component);
             const source = await read(path);
             const processed = await processor.process(source);
             await write(processed);
