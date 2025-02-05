bebab1fe8e638a6817b03485a519caf7fe6fb6bb
jtamatsukuri <62038518+jtamatsukuri@users.noreply.github.com>
2025-02-05 10:57:57 +0900

    Add files via upload

---
+++ src/pages/utokyo_account/utac-initial/ui.js
@@ -0,0 +1,32 @@
+// Select DOM elements to work with
+const signInButton = document.getElementById('signIn');
+const signOutButton = document.getElementById('signOut');
+const titleDiv = document.getElementById('title-div');
+const welcomeDiv = document.getElementById('welcome-div');
+const tableDiv = document.getElementById('table-div');
+const tableBody = document.getElementById('table-body-div');
+
+function welcomeUser(username) {
+    signInButton.classList.add('d-none');
+    signOutButton.classList.remove('d-none');
+    titleDiv.classList.add('d-none');
+    welcomeDiv.classList.remove('d-none');
+    welcomeDiv.innerHTML = `Welcome ${username}!`;
+    window.location = 'https://utelecon.adm.u-tokyo.ac.jp/utokyo_account/mfa/initial/';
+};
+
+function updateTable(account) {
+    tableDiv.classList.remove('d-none');
+    
+    const tokenClaims = createClaimsTable(account.idTokenClaims);
+
+    Object.keys(tokenClaims).forEach((key) => {
+        let row = tableBody.insertRow(0);
+        let cell1 = row.insertCell(0);
+        let cell2 = row.insertCell(1);
+        let cell3 = row.insertCell(2);
+        cell1.innerHTML = tokenClaims[key][0];
+        cell2.innerHTML = tokenClaims[key][1];
+        cell3.innerHTML = tokenClaims[key][2];
+    });
+};
