1630906771dba587c99b4b1d31b994ff61f7f7b0
aymkx <1858589+aymkx@users.noreply.github.com>
2025-03-05 22:16:19 +0900

    modify to reserve information of files in src/pages

--- src/lib/ignoreassets/index.ts
+++ src/lib/ignoreassets/index.ts
@@ -1,12 +1,13 @@
 import {
   type AstroConfig,
   type AstroIntegration,
+  type AstroIntegrationLogger,
   type RedirectConfig,
   type RouteData,
 } from "astro";
 import { glob } from "glob";
 import * as semver from "semver";
-import { extname, parse } from "node:path/posix";
+import { parse, relative, type ParsedPath } from "node:path/posix";
 import { fileURLToPath } from "node:url";
 import { packages } from "../../../package-lock.json";
 
@@ -40,67 +41,7 @@ function checkPackageVersion(name: string, range: string | semver.Range) {
 export default function (extensions: string[]): AstroIntegration {
   checkPackageVersion("astro", "5.0.0 - 5.4.2");
 
-  const extensionsSet = new Set(extensions);
-
-  return {
-    name: "ignore-assets",
-    hooks: {
-      "astro:config:setup": async (options) => {
-        const { config, updateConfig } = options;
-
-        insertPageExtensions(options, extensions);
-
-        const assetsPaths = await enumerateAssets(config, extensions);
-
-        updateConfig({
-          redirects: Object.fromEntries(
-            assetsPaths
-              .filter((path) => config.redirects[path] === undefined)
-              .map((path) => [
-                path,
-                { status: 410, destination: "/" } as unknown as RedirectConfig,
-              ])
-          ),
-        });
-      },
-      "astro:route:setup": (params) => {
-        const { route } = params as unknown as { route: RouteData };
-
-        if (route.type === "page" && route.origin === "project") {
-          const ext = extname(route.component);
-
-          if (extensionsSet.has(ext)) {
-            route.type = "fallback";
-          }
-        }
-      },
-    },
-  };
-}
-
-function insertPageExtensions(options: {}, extensions: string[]) {
-  (
-    options as {
-      addPageExtension?: (input: string[]) => void;
-    }
-  ).addPageExtension?.(extensions);
-}
-
-async function enumerateAssets(
-  config: AstroConfig,
-  extensions: string[]
-): Promise<string[]> {
-  const extensionsSet = new Set(extensions);
-
-  const pages = new URL("./pages", config.srcDir);
-
-  const paths = await glob("**/*", {
-    cwd: fileURLToPath(pages),
-    nodir: true,
-    absolute: false,
-    posix: true,
-  });
-  const pathsSet = new Set(paths);
+  const assetExtensionsSet: ReadonlySet<string> = new Set(extensions);
 
   const pageExtensions = [
     ".astro",
@@ -115,18 +56,117 @@ async function enumerateAssets(
     // ".mdx" is added by @astrojs/mdx integration through addPageExtension(), but
     // there is no way to peek the current value of AstroSettings.pageExtensions.
     ".mdx",
-  ];
+  ] as const;
 
-  const sameNamePageExists = (dirname: string, basename: string) =>
-    pageExtensions.reduce(
-      (acc, ext) => acc || pathsSet.has(`${dirname}/${basename}${ext}`),
-      false
+  let filesInPages: Map<
+    string,
+    {
+      parsedPath: ParsedPath;
+      route: string;
+      collidedPath?: string;
+    }
+  >;
+
+  let pagesDir = "";
+
+  async function configSetupHook(options: {
+    config: AstroConfig;
+    updateConfig: (_: Partial<AstroConfig>) => unknown;
+  }): Promise<void> {
+    pagesDir = fileURLToPath(new URL("./pages", options.config.srcDir));
+
+    insertPageExtensions(options, extensions);
+
+    const paths = (await globPages(pagesDir)).map((path) => ({
+      path,
+      parsedPath: parse(path),
+    }));
+    const assetsInPages = paths.filter(({ parsedPath }) =>
+      assetExtensionsSet.has(parsedPath.ext)
     );
 
-  return paths.flatMap((path) => {
-    const { dir, name, ext } = parse(path);
-    return extensionsSet.has(ext) && !sameNamePageExists(dir, name)
-      ? [`/${dir}/${name}`]
-      : [];
+    filesInPages = new Map(
+      paths.map(({ path, parsedPath }) => [
+        path,
+        { parsedPath, route: `/${parsedPath.dir}/${parsedPath.name}` },
+      ])
+    );
+
+    const sameNamePage = ({ dir, name }: Pick<ParsedPath, "dir" | "name">) =>
+      pageExtensions.reduce<string | undefined>((acc, ext) => {
+        if (acc !== undefined) return acc;
+        const path = `${dir}/${name}${ext}`;
+        return filesInPages.has(path) ? path : undefined;
+      }, undefined);
+
+    for (const { path, parsedPath } of assetsInPages) {
+      const collidedPath = sameNamePage(parsedPath);
+      if (collidedPath) {
+        filesInPages.set(path, {
+          collidedPath,
+          ...filesInPages.get(path)!,
+        });
+      }
+    }
+
+    options.updateConfig({
+      redirects: Object.fromEntries(
+        assetsInPages
+          .filter(({ path }) => {
+            const { route, collidedPath } = filesInPages.get(path)!;
+            return (
+              (options.config.redirects[route] ?? collidedPath) === undefined
+            );
+          })
+          .map(({ path }) => [
+            filesInPages.get(path)!.route,
+            { status: 410, destination: "/" } as unknown as RedirectConfig,
+          ])
+      ),
+    });
+  }
+
+  function routeSetupHook(params: {
+    route: unknown;
+    logger: AstroIntegrationLogger;
+  }) {
+    const route = params.route as RouteData;
+
+    // all routes reachable here are "page" or "endpoint"
+    if (
+      route.origin === "project" &&
+      filesInPages.get(relative(pagesDir, route.component))?.collidedPath !==
+        undefined
+    ) {
+      params.logger.info(
+        `File ${route.component} is set to fallback for route collision`
+      );
+      route.type = "fallback";
+    }
+  }
+
+  return {
+    name: "ignore-assets",
+    hooks: {
+      "astro:config:setup": configSetupHook,
+      "astro:route:setup": routeSetupHook,
+    },
+  };
+}
+
+function insertPageExtensions(options: {}, extensions: string[]) {
+  (
+    options as {
+      addPageExtension?: (input: string[]) => void;
+    }
+  ).addPageExtension?.(extensions);
+}
+
+async function globPages(pagesDir: string): Promise<string[]> {
+  return await glob("**/*", {
+    cwd: pagesDir,
+    nodir: true,
+    absolute: false,
+    posix: true,
   });
 }
