3c1adc1fbd11d50d8b0db90c33db81d78f6e45dd
aymkx <1858589+aymkx@users.noreply.github.com>
2025-03-18 01:13:11 +0900

    modify ExternalLinksIntegration.ts

--- src/lib/ExternalLinksIntegration.ts
+++ src/lib/ExternalLinksIntegration.ts
@@ -6,25 +6,46 @@ import type { Options } from "rehype-external-links";
 import stringify from "rehype-stringify";
 import { read, write } from "to-vfile";
 import { getDistFilePath } from "./util";
+import { EXIT, visit } from "unist-util-visit";
+import type { Root } from "hast";
 
-export default function externalLinks(options: Options): AstroIntegration {
+export default function (options: Options): AstroIntegration {
   const processor = unified()
     .use(parse)
-    .use(rehypeExternalLinks, options)
+    .use(() => {
+      const externalLinks = rehypeExternalLinks(options);
+      return (tree: Root) => {
+        let isRedirectPage = false;
+        visit(tree, (node) => {
+          if (node.type === "element" && node.tagName === "meta") {
+            const httpEquiv = node.properties["httpEquiv"];
+            if (
+              httpEquiv instanceof Array
+                ? httpEquiv.includes("refresh")
+                : httpEquiv === "refresh"
+            ) {
+              isRedirectPage = true;
+              return EXIT;
+            }
+          }
+        });
+        if (!isRedirectPage) externalLinks(tree);
+      };
+    })
     .use(stringify);
 
   return {
-    name: "redirect",
+    name: "external-links",
     hooks: {
       "astro:build:done": async ({ dir, routes }) => {
         await Promise.all(
-          routes.map(async ({ pathname, component }) => {
-            if (!pathname || pathname.endsWith("/rss.xml")) return;
+          routes.map(async ({ pathname, component, type }) => {
+            if (!pathname || type !== "page") return;
             const path = getDistFilePath(dir, pathname, component);
             const source = await read(path);
             const processed = await processor.process(source);
             await write(processed);
-          }),
+          })
         );
       },
     },
