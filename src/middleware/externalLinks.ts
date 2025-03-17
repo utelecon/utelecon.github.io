b843690049692a7addc9d35a61da8f4f3841f891
aymkx <1858589+aymkx@users.noreply.github.com>
2025-03-18 01:21:47 +0900

    add middleware to process external links

---
+++ src/middleware/externalLinks.ts
@@ -0,0 +1,48 @@
+import type { MiddlewareHandler } from "astro";
+import rehypeExternalLinks, {
+  type Options as RehypeExternalLinksOptions,
+} from "rehype-external-links";
+import rehypeParse from "rehype-parse";
+import rehypeStringify from "rehype-stringify";
+import { unified } from "unified";
+
+const rehypeExternalLinksOptions: RehypeExternalLinksOptions = {
+  target: "_blank",
+  rel: ["noopener", "noreferrer"],
+  content: { type: "text", value: "" },
+  contentProperties: { className: ["external-link"] },
+};
+
+const processor = unified()
+  .use(rehypeParse)
+  .use(rehypeExternalLinks, rehypeExternalLinksOptions)
+  .use(rehypeStringify);
+
+const mimeHtmlPattern = /^\s*text\/html(?:[\s;].*|$)/;
+
+const onRequest: MiddlewareHandler = async (_, next) => {
+  const response = await next();
+
+  const contentType = response.headers
+    .entries()
+    .filter(([key]) => key.toLowerCase() === "content-type")
+    .toArray()
+    .pop()?.[1];
+
+  if (contentType?.match(mimeHtmlPattern)) {
+    return new Response(
+      await response
+        .text()
+        .then((html) => processor.process(html))
+        .then(({ value }) => value.toString()),
+      {
+        status: response.status,
+        headers: response.headers,
+      }
+    );
+  }
+
+  return response;
+};
+
+export default onRequest;
