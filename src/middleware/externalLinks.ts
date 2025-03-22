3d2663fb8f5d8bfb8a2ff49359db89eb162b3ada
aymkx <1858589+aymkx@users.noreply.github.com>
2025-03-22 16:36:23 +0900

    modify external links processor as shared

--- src/middleware/externalLinks.ts
+++ src/middleware/externalLinks.ts
@@ -1,11 +1,13 @@
 import type { MiddlewareHandler } from "astro";
 import type { Element } from "hast";
+import { convertElement } from "hast-util-is-element";
 import rehypeExternalLinks, {
   type Options as RehypeExternalLinksOptions,
 } from "rehype-external-links";
 import rehypeParse from "rehype-parse";
 import rehypeStringify from "rehype-stringify";
 import { unified } from "unified";
+import type { Compatible } from "vfile";
 
 type Anchor = Element & { tagName: "a"; properties: { href: string } };
 
@@ -13,41 +15,53 @@ function isAnchor(e: Element): e is Anchor {
   return e.tagName === "a" && typeof e.properties.href === "string";
 }
 
+class ExternalLinkProcessor {
+  hostname?: string;
+  private readonly processor;
+
+  constructor(options: RehypeExternalLinksOptions) {
+    const check = convertElement(options.test);
+
+    const hostnameIsIdentical = (e: Element) => {
+      if (isAnchor(e))
+        try {
+          return new URL(e.properties.href).hostname === this.hostname;
+        } catch {}
+      return false;
+    };
+
+    this.processor = unified()
+      .use(rehypeParse)
+      .use(rehypeExternalLinks, {
+        ...options,
+        // all anchor elements are tested, and
+        // external links which passed are processed.
+        test: (e, i, p) => !hostnameIsIdentical(e) && check(e, i, p),
+      })
+      .use(rehypeStringify);
+  }
+
+  process(file: Compatible | undefined) {
+    return this.processor.process(file);
+  }
+}
+
+const processor = new ExternalLinkProcessor({
+  target: "_blank",
+  rel: ["noopener", "noreferrer"],
+  content: { type: "text", value: "" },
+  contentProperties: { className: ["external-link"] },
+});
+
 const mimeHtmlPattern = /^\s*text\/html(?:[\s;].*|$)/;
 
 export const onRequest: MiddlewareHandler = async (
   { site: { hostname } = {} },
-  _next
+  next
 ) => {
-  const next = _next();
+  processor.hostname = hostname;
 
-  const rehypeExternalLinksOptions: RehypeExternalLinksOptions = {
-    target: "_blank",
-    rel: ["noopener", "noreferrer"],
-    content: { type: "text", value: "" },
-    contentProperties: { className: ["external-link"] },
-  };
-
-  if (hostname) {
-    rehypeExternalLinksOptions.test = (e: Element) => {
-      if (!isAnchor(e)) {
-        // shall be unreachable
-        return true;
-      }
-      try {
-        return new URL(e.properties.href).hostname !== hostname;
-      } catch {
-        return true;
-      }
-    };
-  }
-
-  const processor = unified()
-    .use(rehypeParse)
-    .use(rehypeExternalLinks, rehypeExternalLinksOptions)
-    .use(rehypeStringify);
-
-  const response = await next;
+  const response = await next();
 
   if (response.headers.get("content-type")?.match(mimeHtmlPattern)) {
     return new Response(
