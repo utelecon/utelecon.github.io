6f692185467a367b3146add88a529604cbd4a809
seieric <30688855+seieric@users.noreply.github.com>
2025-12-14 18:33:17 +0900

    add: Microsoft Clarity

--- astro.config.ts
+++ astro.config.ts
@@ -73,7 +73,7 @@ export default defineConfig({
     copyAsset(),
     partytown({
       config: {
-        forward: ["dataLayer.push"],
+        forward: ["dataLayer.push", "clarity"],
       },
     }),
     ignoreAssets([
