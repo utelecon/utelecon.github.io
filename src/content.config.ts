fb7b2210645f69a727dcec6b4e3c0afdd945d326
haxibami <me@haxibami.net>
2025-12-22 19:03:25 +0900

    use content collection for emergencies

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,6 +2,24 @@ import { defineCollection } from "astro:content";
 import { z } from "astro/zod"
 import { glob } from "astro/loaders";
 
+const emergencies = defineCollection({
+  loader: glob({pattern: "*.{md,mdx}", base: "./src/emergencies" }),
+  schema: z.object({
+    pattern: z.string().refine((v) => {
+      try {
+        new RegExp(v);
+        return true;
+      }
+      catch (e) {
+        return false;
+      }
+    })
+    .transform(
+      (v) => new RegExp(v)
+    )
+  })
+})
+
 const events = defineCollection({
   loader: glob({pattern: ["events/*/index.{md,mdx}", "events/*.{md,mdx}"], base: "./src/pages" }),
   schema: z.object({
@@ -9,4 +27,4 @@ const events = defineCollection({
   })
 })
 
-export const collections = { events };
+export const collections = { emergencies, events };
